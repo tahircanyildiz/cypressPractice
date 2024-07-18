26b11058f27d7fb32b42288dc8091c538f4d748f
tahircanyildiz <tahircanyildiz@hotmail.com>
2024-07-18 10:31:35 +0300

    back forward added

---
+++ cypress/e2e/cypress-egitim/cypressBackForward.cy.js
@@ -0,0 +1,23 @@
+describe('Back Forward Kullanimi', () => {
+    it('cypress.io sayfasında ileri geri', () => {
+        cy.visit('https://www.cypress.io/')
+   
+
+    cy.get('nav > div > ul > li:nth-child(5) > a').click()
+    cy.title().should('eq', "Scale Open-Source Testing with Cypress Cloud | Cypress Cloud Pricing")
+
+   cy.go("back") // geri dön
+   cy.title().should('eq', "Testing Frameworks for Javascript | Write, Run, Debug | Cypress")
+
+   cy.go("forward")
+   cy.title().should('eq', "Scale Open-Source Testing with Cypress Cloud | Cypress Cloud Pricing")
+
+   cy.go(-1)
+   cy.title().should('eq', "Testing Frameworks for Javascript | Write, Run, Debug | Cypress")
+
+
+});
+
+
+
+});
--- cypress/e2e/cypress-egitim/htmlElementLocate.cy.js
+++ cypress/e2e/cypress-egitim/htmlElementLocate.cy.js
@@ -1,6 +1,6 @@
 describe('Cypress Locator', () => {
     it('html Element Locate Etme', () => {
-        cy.get('#user-name') // id locate
+        cy.get('header#header img') // id locate
         cy.get('.login-box') // class locate
         cy.get('input[name:"user-name"]') // attribute locate
         cy.get('a:contains("Login")')
