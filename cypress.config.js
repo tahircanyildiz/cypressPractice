136836b5da4784d2962cc661d2d70c0c0fc09684
tahircanyildiz <tahircanyildiz@hotmail.com>
2024-08-06 16:37:11 +0300

    enviorment variable added

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,28 @@
-const { defineConfig } = require('cypress')
-const {downloadFile} = require('cypress-downloadfile/lib/addPlugin')
+const { defineConfig } = require("cypress");
 const { removeDirectory } = require('cypress-delete-downloads-folder');
 
+
 module.exports = defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
+  projectId: "ozwz47",
+  viewportHeight: 1200,
+  viewportWidth: 1500,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-         on('task', {removeDirectory })
-      },
-      env:{
-        hideXhr:true
-      }
-    }
-})
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
+      on('task', { removeDirectory })
+    },
+    env: {
+      hideXhr: true,
+      amazon: "https://www.amazon.de",
+      google: "https://www.google.com",
+      saucedemo: "https://www.saucedemo.com"
+    },
+    specPattern: 'cypress/e2e/**/*.{js, jsx, ts, tsx}',
+    experimentalWebKitSupport: true,
+    experimentalRunAllSpecs: true,
+    watchForFileChanges: false,
+    experimentalStudio: true
+  },
+});
