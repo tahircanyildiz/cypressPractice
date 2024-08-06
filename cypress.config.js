bf50ec9a2d74cb54cca883848675e512616544ef
tahircanyildiz <tahircanyildiz@hotmail.com>
2024-08-06 21:23:20 +0300

    Mochawesome-reporter added

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
   viewportHeight: 1200,
   viewportWidth: 1500,
   reporter: 'cypress-mochawesome-reporter',
+  video:true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
